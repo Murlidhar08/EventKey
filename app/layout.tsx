0d618d11c5b9c84e6db7227d12a999f142be3d29
Murlidhar08 <murlidharproperties08@gmail.com>
2026-09-15 11:33:32 +0530

    update icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,10 +50,10 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "images/app-icon/favicon-16.png", sizes: "16x16", type: "image/png" },
-      { url: "images/app-icon/favicon-32.png", sizes: "32x32", type: "image/png" },
-      { url: "images/logo/maskable_icon_x192.png", sizes: "192x192", type: "image/png" },
-      { url: "images/logo/maskable_icon_x512.png", sizes: "512x512", type: "image/png" },
+      { url: "/images/app-icon/favicon-16.png", sizes: "16x16", type: "image/png" },
+      { url: "/images/app-icon/favicon-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/images/logo/maskable_icon_x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/images/logo/maskable_icon_x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: "images/logo/maskable_icon_x192.png",
   },
